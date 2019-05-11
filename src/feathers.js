72c373727a5fcb099d85fb023f162e927fe75a02
Rubita <rubita.kumari@gmail.com>
2019-05-11 20:11:03 +0530

    storage switched to window

--- src/feathers.js
+++ src/feathers.js
@@ -1,13 +1,14 @@
 import feathers from '@feathersjs/feathers';
 import localstorage from 'feathers-localstorage';
-import localforage from 'localforage';
+// import localforage from 'localforage';
 
 import pokedex from './pokedex.json';
 
 export const app = feathers();
 
 app.use('/pokemons', localstorage({
-  storage: localforage,
+  // storage: localforage,
+  storage: window.localStorage,
   paginate: { default: 10, max: 50 },
   multi: true,
 }));
